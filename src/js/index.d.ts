2230d095cc3dc6b7790a32b8956828d4ceed116c
Jaco Greeff <jacogr@gmail.com>
2019-03-14 21:24:26 +0100

    Cleanup duplicate exports

--- src/js/index.d.ts
+++ src/js/index.d.ts
@@ -1,4 +1,3 @@
-export function softDerivePublic (publicKey: Uint8Array, chainCode: Uint8Array): Uint8Array;
 export function isReady (): boolean;
 export function keypairFromSeed (seed: Uint8Array): Uint8Array;
 export function secretFromSeed (seed: Uint8Array): Uint8Array;
