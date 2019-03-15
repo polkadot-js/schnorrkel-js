19945fe7597e07620503f0000e215a45e0d1a842
Jaco Greeff <jacogr@gmail.com>
2019-03-15 17:16:11 +0100

    Expand tests

--- test/_test.js
+++ test/_test.js
@@ -12,7 +12,7 @@ function extractKeys (pair) {
 
 function randomPair () {
   const seed = crypto.randomBytes(32);
-  const pair = schnorrkel.keypair_from_seed(seed);
+  const pair = schnorrkel.keypairFromSeed(seed);
 
   assert(pair.length === 96, 'ERROR: Invalid pair created');
 
@@ -37,7 +37,7 @@ async function pairFromSeed () {
     '741c08a06f41c596608f6774259bd9043304adfa5d3eea62760bd9be97634d63'
   );
 
-  const pair = schnorrkel.keypair_from_seed(SEED);
+  const pair = schnorrkel.keypairFromSeed(SEED);
 
   console.error('pairFromSeed');
   console.log('\t', u8aToHex(pair.slice(0, 64)));
@@ -56,8 +56,8 @@ async function verifyExisting () {
   const MESSAGE = stringToU8a('this is a message');
   const SIGNATURE = hexToU8a(
     '0x' +
-    '162657aac408f204381e7c491cc67c61c454c169f22841f7d949c4b63c79c83e' +
-    '35c8d79dfca6c9f1778d5a91391bad7753edfb714518b2be58f33322712fe20a'
+    'decef12cf20443e7c7a9d406c237e90bcfcf145860722622f92ebfd5eb4b5b39' +
+    '90b6443934b5cba8f925a0ae75b3a77d35b8490cbb358dd850806e58eaf72904'
   );
 
   const isValid = schnorrkel.verify(SIGNATURE, MESSAGE, PK);
@@ -89,6 +89,62 @@ async function signAndVerify () {
   console.timeEnd('signAndVerify');
 }
 
+async function deriveHard () {
+  console.time('deriveHard');
+  console.log();
+
+  const CC = hexToU8a('0x0c666f6f00000000000000000000000000000000000000000000000000000000');
+  const [pair] = randomPair();
+  const derived = schnorrkel.hardDeriveKeypair(pair, CC);
+
+  console.error('deriveHard');
+  console.log('\t', u8aToHex(derived.slice(0, 64)));
+  console.log('\t', u8aToHex(derived.slice(64)));
+
+  console.timeEnd('deriveHard');
+}
+
+async function deriveSoft () {
+  console.time('deriveSoft');
+  console.log();
+
+  const CC = hexToU8a('0x0c666f6f00000000000000000000000000000000000000000000000000000000');
+
+  const [pair] = randomPair();
+  const derived = schnorrkel.softDeriveKeypair(pair, CC);
+
+  console.error('deriveSoft');
+  console.log('\t', u8aToHex(derived.slice(0, 64)));
+  console.log('\t', u8aToHex(derived.slice(64)));
+
+  console.timeEnd('deriveSoft');
+}
+
+async function deriveSoftKnown () {
+  console.time('deriveSoftKnown');
+  console.log();
+
+  const CC = hexToU8a('0x0c666f6f00000000000000000000000000000000000000000000000000000000');
+  const PUBLIC = '0x40b9675df90efa6069ff623b0fdfcf706cd47ca7452a5056c7ad58194d23440a';
+  const PAIR = hexToU8a(
+    '0x' +
+    '28b0ae221c6bb06856b287f60d7ea0d98552ea5a16db16956849aa371db3eb51' +
+    'fd190cce74df356432b410bd64682309d6dedb27c76845daf388557cbac3ca34' +
+    '46ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a'
+  );
+
+  const derived = schnorrkel.softDeriveKeypair(PAIR, CC);
+  const publicKey = u8aToHex(derived.slice(64));
+
+  console.error('deriveSoftKnown');
+  console.log('\t', u8aToHex(derived.slice(0, 64)));
+  console.log('\t', publicKey);
+
+  assert(publicKey === PUBLIC, 'Unmatched resulting public keys');
+
+  console.timeEnd('deriveSoftKnown');
+}
+
 async function benchmark () {
   console.time('benchmark');
   console.log();
@@ -110,7 +166,10 @@ async function benchmark () {
 (async () => {
   await beforeAll();
   await pairFromSeed();
-  await verifyExisting();
   await signAndVerify();
+  await verifyExisting();
+  await deriveHard();
+  await deriveSoft();
+  await deriveSoftKnown();
   await benchmark();
 })().catch(console.log).finally(() => process.exit());
